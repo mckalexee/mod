2197faa8d9cd822e160cc3eb8594c144a6877494
Alex McKenzie <mckenzie@tailwindapp.com>
2021-01-09 19:57:36 -0500

    add color shader

--- index.js
+++ index.js
@@ -35,6 +35,37 @@ let fs = `
     // gl_FragColor = vec4(normal.x, normal.y, 0, 1.0);
   }
 `
+let fsColorBands = `
+  #define PI 3.1415926538
+  precision highp float;
+  varying vec2 vpos;
+  uniform float canvas_x;
+  uniform float canvas_y;
+  uniform float base;
+  uniform float shift;
+
+  void main (void) {
+    float normal_x = (vpos.x + 1.0) * canvas_x / 2.0;
+    float normal_y = (vpos.y + 1.0) * canvas_y / 2.0;
+    float color_value_r = mod(normal_x * normal_y + shift, base);
+    float color_value_g = mod(normal_x * normal_y + shift + (base / 3.0), base);
+    float color_value_b = mod(normal_x * normal_y + shift + (2.0 * base / 3.0), base);
+    float color_value_normal_r = color_value_r / (base - 1.0);
+    float color_value_normal_g = color_value_g / (base - 1.0);
+    float color_value_normal_b = color_value_b / (base - 1.0);
+    float color_value_rad_r  = color_value_normal_r * PI;
+    float color_value_rad_g  = color_value_normal_g * PI;
+    float color_value_rad_b  = color_value_normal_b * PI;
+    float color_value_sin_r  = (sin(color_value_rad_r) - 0.9) * 10.0;
+    float color_value_sin_g  = (sin(color_value_rad_g) - 0.9) * 10.0;
+    float color_value_sin_b  = (sin(color_value_rad_b) - 0.9) * 10.0;
+
+
+    gl_FragColor = vec4(color_value_sin_r, color_value_sin_g, color_value_sin_b, 1.0);
+    // gl_FragColor = vec4(normal.x, normal.y, 0, 1.0);
+  }
+`
+
 
 
 let vertexShader = gl.createShader(gl.VERTEX_SHADER)
@@ -43,6 +74,7 @@ gl.compileShader(vertexShader);
 
 let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 gl.shaderSource(fragmentShader, fs);
+// gl.shaderSource(fragmentShader, fsColorBands);
 gl.compileShader(fragmentShader);
 
 let program = gl.createProgram();
@@ -77,7 +109,7 @@ let base = 1024;
 let shift = 0;
 
 function animate() {
-  shift = (shift + 1) % base;
+  shift = (shift + 2) % base;
 
   const width = window.innerWidth;
   const height = window.innerHeight;
